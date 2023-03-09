6e96d0736aee5e3665b50babe4ca7c0527f75c24
Garvit Agarwal <garvitakagarvit@gmail.com>
2023-03-09 14:31:40 +0530

    injection script

--- injection.py
+++ injection.py
@@ -192,169 +192,166 @@ for n in range(len(catalogue)-3):
     line = catalogue[i].split('\t')
 
     print(line[0])
-    # num_moons = int(line[2])
+    num_moons = int(line[2])
 
-    # kepler_lightcurve_file = line[1]
-    # f = open("/home/garvit/Downloads/Exomoon_project/good_kepler_data/{}".format(kepler_lightcurve_file), 'r')
-    # kepler_data = f.readlines()
-    # f.close()
-    # kepler_starttime = float(kepler_data[1].split('\t')[0])
-    # kepler_endtime = float(kepler_data[-1].split('\t')[0])
-    # data_time_interval = kepler_endtime - kepler_starttime
+    kepler_lightcurve_file = line[1]
+    f = open("/home/garvit/Downloads/Exomoon_project/good_kepler_data/{}".format(kepler_lightcurve_file), 'r')
+    kepler_data = f.readlines()
+    f.close()
+    kepler_starttime = float(kepler_data[1].split('\t')[0])
+    kepler_endtime = float(kepler_data[-1].split('\t')[0])
+    data_time_interval = kepler_endtime - kepler_starttime
 
-    # N = len(kepler_data) -1
-    # kepler_flux = np.ndarray((N))
-    # timestamps = np.ndarray((N))
-    # flux_error = np.ndarray((N))
-    # quarter_arr = np.ndarray((N))
+    N = len(kepler_data) -1
+    kepler_flux = np.ndarray((N))
+    timestamps = np.ndarray((N))
+    flux_error = np.ndarray((N))
+    quarter_arr = np.ndarray((N))
 
-    # for j in range(N):
-    #     timestamps[j] = float(kepler_data[j+1].split('\t')[0])
-    #     kepler_flux[j] = float(kepler_data[j+1].split('\t')[1])
-    #     flux_error[j] = float(kepler_data[j+1].split('\t')[2])
-    #     quarter_arr[j] = int(kepler_data[j+1].split('\t')[4])
+    for j in range(N):
+        timestamps[j] = float(kepler_data[j+1].split('\t')[0])
+        kepler_flux[j] = float(kepler_data[j+1].split('\t')[1])
+        flux_error[j] = float(kepler_data[j+1].split('\t')[2])
+        quarter_arr[j] = int(kepler_data[j+1].split('\t')[4])
 
-    # quarter_list = list(dict.fromkeys(quarter_arr))
-    # num_quarters = len(quarter_list)
-    # quarters = []
-    # for j in quarter_list:
-    #     idxs = np.where(quarter_arr == j)[0]
-    #     if(idxs.shape[0]==1): 
-    #         num_quarters -= 1
-    #         continue
-    #     quarters.append(idxs)
+    quarter_list = list(dict.fromkeys(quarter_arr))
+    num_quarters = len(quarter_list)
+    quarters = []
+    for j in quarter_list:
+        idxs = np.where(quarter_arr == j)[0]
+        if(idxs.shape[0]==1): 
+            num_quarters -= 1
+            continue
+        quarters.append(idxs)
 
-    # system_file = line[0].split('_')
-    # date, res_nores, sim_no = system_file[0], system_file[1], system_file[2]
-    # f = open("/media/Datas/Exomoon_project/final_model_LCs/{}_{}_{}.txt".format(sim_no, res_nores, date), 'r')
-    # model_data = f.readlines()
-    # f.close()
+    system_file = line[0].split('_')
+    date, res_nores, sim_no = system_file[0], system_file[1], system_file[2]
+    f = open("/media/Datas/Exomoon_project/final_model_LCs/{}_{}_{}.txt".format(sim_no, res_nores, date), 'r')
+    model_data = f.readlines()
+    f.close()
 
-    # flux_planet_allmoons = np.ndarray((len(model_data) - 1))
-    # time = np.ndarray((len(model_data)-1))
-    # flux_moons = np.ndarray((num_moons, len(model_data)-1))
+    flux_planet_allmoons = np.ndarray((len(model_data) - 1))
+    time = np.ndarray((len(model_data)-1))
+    flux_moons = np.ndarray((num_moons, len(model_data)-1))
 
-    # for j in range(len(model_data)-1):
-    #     time[j] = float(model_data[j+1].split('\t')[0])
-    #     flux_planet_allmoons[j] = float(model_data[j+1].split('\t')[1])
-    #     for k in range(num_moons):
-    #         flux_moons[k][j] = float(model_data[j+1].split('\t')[2+k])
+    for j in range(len(model_data)-1):
+        time[j] = float(model_data[j+1].split('\t')[0])
+        flux_planet_allmoons[j] = float(model_data[j+1].split('\t')[1])
+        for k in range(num_moons):
+            flux_moons[k][j] = float(model_data[j+1].split('\t')[2+k])
 
-    # best_offset = float(line[16])
-    # time_kepler = time - time[0] + kepler_starttime + best_offset
-    # model_time_interval = time[-1] - time[0]
+    best_offset = float(line[16])
+    time_kepler = time - time[0] + kepler_starttime + best_offset
+    model_time_interval = time[-1] - time[0]
 
-    # flux_planet_allmoons_temp = flux_planet_allmoons.copy()
-    # flux_moons_temp = flux_moons.copy()
-    # time_temp = time.copy()
-    # if(data_time_interval < model_time_interval + best_offset):
-    #     flux_planet_allmoons_temp = flux_planet_allmoons[time_kepler < kepler_endtime]
-    #     flux_moons_temp = flux_moons[:, time_kepler < kepler_endtime]
-    #     time_temp = time[time_kepler < kepler_endtime]
-    #     time_kepler = time_kepler[time_kepler < kepler_endtime]
+    flux_planet_allmoons_temp = flux_planet_allmoons.copy()
+    flux_moons_temp = flux_moons.copy()
+    time_temp = time.copy()
+    if(data_time_interval < model_time_interval + best_offset):
+        flux_planet_allmoons_temp = flux_planet_allmoons[time_kepler < kepler_endtime]
+        flux_moons_temp = flux_moons[:, time_kepler < kepler_endtime]
+        time_temp = time[time_kepler < kepler_endtime]
+        time_kepler = time_kepler[time_kepler < kepler_endtime]
 
-    # flux_total = np.copy(flux_planet_allmoons_temp) 
-    # for j in range(num_moons): flux_total+= flux_moons_temp[j]
-    # flux_total-=(num_moons)
-    # #plt.scatter(time_kepler, flux_total, s=0.5)
-    # # plt.show()
-
-    # TTs = model_data[0].split('\t')[:-1]
-    # num_epochs = len(TTs)
-    # TT_planet_allmoons = np.ndarray((num_epochs))
-    # for j in range(num_epochs):
-    #     TT_planet_allmoons[j] = TTs[j]
-
-    # TT_kepler = TT_planet_allmoons - time[0] + kepler_starttime + best_offset
-    # TT_kepler = TT_kepler[TT_kepler < kepler_endtime]
-    # num_epochs = TT_kepler.shape[0]
-    # # print(TT_kepler)
-
-    # injected_flux, rebinned_time = inject()
-    # # injected_flux = kepler_flux.copy()
-    # print("injection complete\n")
-
-    # transit_duration = float(line[17])
-    # per_bary = float(line[3])
-    
-    # trends = [0 for j in range(num_quarters)]
-    # detrended_fluxes = [0 for j in range(num_quarters)]
-    # for j in range(num_quarters):
-    #     mask_idxs = mask_indices(timestamps[quarters[j]])
-    #     trends[j], detrended_fluxes[j], errors_detrend = cofiam_detrend(times=timestamps[quarters[j]], fluxes=injected_flux[quarters[j]], errors=flux_error[quarters[j]], mask_idxs=mask_idxs, max_degree=30)
-
-    # trend = np.concatenate(trends)
-    # detrended_flux = np.concatenate(detrended_fluxes)
-    # print("detrending complete\n")
-    
-    # # plt.style.reload_library()
-    # # plt.style.use(['science', 'bright', 'grid'])
-    # plt.scatter(timestamps, injected_flux, s=0.5)
-    # plt.scatter(timestamps, trend, s=0.5)
-    # plt.xlabel('Time (days)')
-    # plt.ylabel('Raw flux value')
+    flux_total = np.copy(flux_planet_allmoons_temp) 
+    for j in range(num_moons): flux_total+= flux_moons_temp[j]
+    flux_total-=(num_moons)
+    #plt.scatter(time_kepler, flux_total, s=0.5)
     # plt.show()
 
-    # final_lc = np.ndarray((rebinned_time.shape[0]))
-    # for j in range(rebinned_time.shape[0]):
-    #     final_lc[j] = detrended_flux[np.where(timestamps == rebinned_time[j])]
-    # plt.scatter(rebinned_time, final_lc, s=0.5, color='black')
-    # plt.show()
+    TTs = model_data[0].split('\t')[:-1]
+    num_epochs = len(TTs)
+    TT_planet_allmoons = np.ndarray((num_epochs))
+    for j in range(num_epochs):
+        TT_planet_allmoons[j] = TTs[j]
 
-    # while(True):
-    #     timestamps_excl = timestamps.copy()
-    #     injected_flux_excl = injected_flux.copy()
-    #     trend_excl = trend.copy()
-    #     detrended_flux_excl = detrended_flux.copy()
-    #     rebinned_time_excl = rebinned_time.copy()
+    TT_kepler = TT_planet_allmoons - time[0] + kepler_starttime + best_offset
+    TT_kepler = TT_kepler[TT_kepler < kepler_endtime]
+    num_epochs = TT_kepler.shape[0]
+    # print(TT_kepler)
 
-    #     num = int(input("number of regions to exclude: \n"))
-    #     for j in range(num):
-    #         start = float(input("start time: \n"))
-    #         end = float(input("end time: \n"))
-    #         indices = np.where((timestamps_excl < start) | (timestamps_excl > end))
-    #         injected_flux_excl = injected_flux_excl[indices]
-    #         trend_excl = trend_excl[indices]
-    #         detrended_flux_excl = detrended_flux_excl[indices]
-    #         timestamps_excl = timestamps_excl[indices]
-    #         rebinned_time_excl = rebinned_time_excl[np.where((rebinned_time_excl < start) | (rebinned_time_excl > end))]
+    injected_flux, rebinned_time = inject()
+    print("injection complete\n")
+
+    transit_duration = float(line[17])
+    per_bary = float(line[3])
+    
+    trends = [0 for j in range(num_quarters)]
+    detrended_fluxes = [0 for j in range(num_quarters)]
+    for j in range(num_quarters):
+        mask_idxs = mask_indices(timestamps[quarters[j]])
+        trends[j], detrended_fluxes[j], errors_detrend = cofiam_detrend(times=timestamps[quarters[j]], fluxes=injected_flux[quarters[j]], errors=flux_error[quarters[j]], mask_idxs=mask_idxs, max_degree=30)
+
+    trend = np.concatenate(trends)
+    detrended_flux = np.concatenate(detrended_fluxes)
+    print("detrending complete\n")
+    
+    plt.scatter(timestamps, injected_flux, s=0.5)
+    plt.scatter(timestamps, trend, s=0.5)
+    plt.xlabel('Time (days)')
+    plt.ylabel('Raw flux value')
+    plt.show()
+
+    final_lc = np.ndarray((rebinned_time.shape[0]))
+    for j in range(rebinned_time.shape[0]):
+        final_lc[j] = detrended_flux[np.where(timestamps == rebinned_time[j])]
+    plt.scatter(rebinned_time, final_lc, s=0.5, color='black')
+    plt.show()
+
+    while(True):
+        timestamps_excl = timestamps.copy()
+        injected_flux_excl = injected_flux.copy()
+        trend_excl = trend.copy()
+        detrended_flux_excl = detrended_flux.copy()
+        rebinned_time_excl = rebinned_time.copy()
+
+        num = int(input("number of regions to exclude: \n"))
+        for j in range(num):
+            start = float(input("start time: \n"))
+            end = float(input("end time: \n"))
+            indices = np.where((timestamps_excl < start) | (timestamps_excl > end))
+            injected_flux_excl = injected_flux_excl[indices]
+            trend_excl = trend_excl[indices]
+            detrended_flux_excl = detrended_flux_excl[indices]
+            timestamps_excl = timestamps_excl[indices]
+            rebinned_time_excl = rebinned_time_excl[np.where((rebinned_time_excl < start) | (rebinned_time_excl > end))]
 
 
-    #     plt.scatter(timestamps_excl, detrended_flux_excl, s=0.5, color='black')
-    #     plt.show()        
+        plt.scatter(timestamps_excl, detrended_flux_excl, s=0.5, color='black')
+        plt.show()        
 
-    #     final_lc = np.ndarray((rebinned_time_excl.shape[0]))
-    #     for j in range(rebinned_time_excl.shape[0]):
-    #         final_lc[j] = detrended_flux_excl[np.where(timestamps_excl == rebinned_time_excl[j])]
-    #     plt.scatter(rebinned_time_excl, final_lc, s=0.5, color='black')
-    #     plt.show()
+        final_lc = np.ndarray((rebinned_time_excl.shape[0]))
+        for j in range(rebinned_time_excl.shape[0]):
+            final_lc[j] = detrended_flux_excl[np.where(timestamps_excl == rebinned_time_excl[j])]
+        plt.scatter(rebinned_time_excl, final_lc, s=0.5, color='black')
+        plt.show()
 
-    #     residual = injected_flux_excl - trend_excl
-    #     plt.scatter(timestamps_excl, residual, s=0.5, color='black')
-    #     plt.show()
+        residual = injected_flux_excl - trend_excl
+        plt.scatter(timestamps_excl, residual, s=0.5, color='black')
+        plt.show()
 
-    #     mask_idxs = mask_indices(timestamps_excl)
-    #     masked_residual = np.delete(residual, mask_idxs)
-    #     plt.scatter(np.arange(masked_residual.shape[0]), masked_residual, s=0.5, color='black')
-    #     plt.show()
+        mask_idxs = mask_indices(timestamps_excl)
+        masked_residual = np.delete(residual, mask_idxs)
+        plt.scatter(np.arange(masked_residual.shape[0]), masked_residual, s=0.5, color='black')
+        plt.show()
 
-    #     done = input("Exclusion Done?\n")
-    #     if(done == 'yes'): break
+        done = input("Exclusion Done?\n")
+        if(done == 'yes'): break
         
     
-    # rebinned_time = rebinned_time_excl
+    rebinned_time = rebinned_time_excl
 
-    # dw_statistic = stattools.durbin_watson(masked_residual[np.logical_not(np.isnan(masked_residual))])
-    # print(dw_statistic, float(line[15]))
+    dw_statistic = stattools.durbin_watson(masked_residual[np.logical_not(np.isnan(masked_residual))])
+    print(dw_statistic, float(line[15]))
     
     
-    # print("Writing the injected light curve for this system in a file\n-----------------------------------------------------\n")
-    # f = open('/home/garvit/Downloads/Exomoon_project/final_lightcurves/final_lightcurve_{}_{}_{}.csv'.format(date, res_nores, sim_no), 'w')
-    # f.write("System architecture file: {} {} {}\n".format(date, res_nores, sim_no))
-    # f.write("Kepler light curve file: {}\n".format(kepler_lightcurve_file))
-    # writer1 = csv.writer(f, delimiter=',')
-    # writer1.writerows(zip(rebinned_time, final_lc))
-    # f.close()
+    print("Writing the injected light curve for this system in a file\n-----------------------------------------------------\n")
+    f = open('/home/garvit/Downloads/Exomoon_project/final_lightcurves/final_lightcurve_{}_{}_{}.csv'.format(date, res_nores, sim_no), 'w')
+    f.write("System architecture file: {} {} {}\n".format(date, res_nores, sim_no))
+    f.write("Kepler light curve file: {}\n".format(kepler_lightcurve_file))
+    writer1 = csv.writer(f, delimiter=',')
+    writer1.writerows(zip(rebinned_time, final_lc))
+    f.close()
 
 
     
